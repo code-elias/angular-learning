4de66124eeff1b76094202f5e9b7b63a4642e185
code-Elias <eligolam.mahfuzul.dev@gmail.com>
2025-01-13 18:53:54 +0100

    Created logo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
 import { HeaderLinksComponent } from '../header-links/header-links.component'
+import { HeaderTitleComponent } from '../header-title/header-title.component'
 
 @Component({
   selector: 'app-header',
-  imports: [HeaderLinksComponent],
+  imports: [HeaderLinksComponent, HeaderTitleComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
